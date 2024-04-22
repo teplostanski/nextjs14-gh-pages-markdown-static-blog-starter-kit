720e603f86a8d7b5dec49cb04242f7cd2d48fbd9
teplostanski <teplostanski@yandex.ru>
2024-04-22 06:51:04 +0300

    test commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ const repoName = process.env.NEXT_PUBLIC_REPO_NAME || '';
 const basePath = repoName ? `/${repoName}` : '';
 const assetPrefix = repoName ? `/${repoName}/` : '';
 
+console.log("Base Path:", process.env.NEXT_PUBLIC_REPO_NAME);
+
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   basePath,
