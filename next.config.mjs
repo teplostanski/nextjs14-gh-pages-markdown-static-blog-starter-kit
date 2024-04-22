2d59fe0fb4e1138fb574d857cb87c19d426ad1d3
teplostanski <teplostanski@yandex.ru>
2024-04-22 07:02:56 +0300

    test commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,14 @@
 const repoName = process.env.NEXT_PUBLIC_REPO_NAME || '';
 
 const basePath = repoName ? `/${repoName}` : '';
+const assetPrefix = repoName ? `/${repoName}/` : '';
 
 console.log("Base Path:", process.env.NEXT_PUBLIC_REPO_NAME);
 
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   basePath: basePath,
+  assetPrefix: assetPrefix,
   output: "export",
 }
 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-const repoName = process.env.NEXT_PUBLIC_GH_REPO_DEPLOY_WITHOUT_CUSTOM_DOMAIN
+const repoName = process.env.NEXT_PUBLIC_REPO_NAME
 
 export const basePath = repoName ? `/${repoName}` : ''
 export const assetPrefix = repoName ? `/${repoName}/` : ''
