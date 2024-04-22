9ac614fbbe5ad254636efef16ea59f5c4f286df0
teplostanski <teplostanski@yandex.ru>
2024-04-22 05:18:19 +0300

    test commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const ON_GITHUB_PAGES = process.env.NODE_ENV === 'production'
-
 // replace this with your repository name
 const repoName = 'static-recipe-blog'
 
-const basePath = ON_GITHUB_PAGES ? `/${repoName}` : ''
-const assetPrefix = ON_GITHUB_PAGES ? `/${repoName}/` : ''
+const basePath = `/${repoName}`
+const assetPrefix = `/${repoName}/`
 
 const nextConfig = {
   reactStrictMode: true,
