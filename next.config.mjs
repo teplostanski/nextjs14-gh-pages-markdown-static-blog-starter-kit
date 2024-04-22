3d0f8a5c9a04aa03794fe08dc8e169f70cafcaf2
teplostanski <teplostanski@yandex.ru>
2024-04-22 05:21:32 +0300

    test commit

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  output: 'export', // This option must be set for static exports.
+}
+
+export default nextConfig
